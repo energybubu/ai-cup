4e355d93cb19c18fa497e1015fb25bce818d9eda
energybubu <ivanhoward011878@gmail.com>
2024-10-22 10:25:20 +0000

    feat: add  default arguments

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Evaluate the model.")
     parser.add_argument(
@@ -18,44 +19,43 @@ def parse_arguments():
     parser.add_argument(
         "--gt_path",
         type=str,
-        required=True,
+        default="datasets/dataset/preliminary/ground_truths_example.json",
         help="The path to the ground truth file.",
     )
 
-    return parser.parse_args() 
+    return parser.parse_args()
+
 
 def evaluate(args):
     with open(args.question_path) as f:
-        qs_ref = json.load(f)['questions']
-        sources = {
-            q['qid']: q['source'] for q in qs_ref
-        }
+        qs_ref = json.load(f)["questions"]
+        sources = {q["qid"]: q["source"] for q in qs_ref}
 
     with open(args.pred_path) as f:
-        pred = json.load(f)['answers']
-        pred = {
-            p['qid']: p['retrieve'] for p in pred
-        }
-        
-    with open(args.gt_path) as f:
-        gt = json.load(f)['ground_truths']
-        gt = {
-            g['qid']: g['retrieve'] for g in gt
-        }
+        pred = json.load(f)["answers"]
+        pred = {p["qid"]: p["retrieve"] for p in pred}
 
-    assert set(pred.keys()) == set(gt.keys()), "Prediction and ground truth have different qids."
-    assert len(pred) == len(gt) == 150, "Prediction and ground truth have wrong number of qids."
+    with open(args.gt_path) as f:
+        gt = json.load(f)["ground_truths"]
+        gt = {g["qid"]: g["retrieve"] for g in gt}
+
+    assert set(pred.keys()) == set(
+        gt.keys()
+    ), "Prediction and ground truth have different qids."
+    assert (
+        len(pred) == len(gt) == 150
+    ), "Prediction and ground truth have wrong number of qids."
 
     correct = 0
     for qid in pred.keys():
-        assert pred[qid] in sources[qid], f"Prediction with qid {qid} does not exist in the sources."
+        assert (
+            pred[qid] in sources[qid]
+        ), f"Prediction with qid {qid} does not exist in the sources."
         if pred[qid] == gt[qid]:
             correct += 1
 
     print(f"Accuracy: {correct/len(pred):.2f}")
 
-    
-    
 
 if __name__ == "__main__":
     args = parse_arguments()
