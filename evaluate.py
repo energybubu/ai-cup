b0448120d0b2c3c33c4f55add1e7b1c1801b5e19
JasonTuTW <b09902138@csie.ntu.edu.tw>
2024-10-28 07:52:02 +0000

    include more info in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -32,12 +32,13 @@ def evaluate(args):
         sources = {q["qid"]: q["source"] for q in qs_ref}
 
     with open(args.pred_path) as f:
-        pred = json.load(f)["answers"]
-        pred = {p["qid"]: p["retrieve"] for p in pred}
+        pred_data = json.load(f)["answers"]
+        pred = {p["qid"]: p["retrieve"] for p in pred_data}
 
     with open(args.gt_path) as f:
-        gt = json.load(f)["ground_truths"]
-        gt = {g["qid"]: g["retrieve"] for g in gt}
+        gt_data = json.load(f)["ground_truths"]
+        gt = {g["qid"]: g["retrieve"] for g in gt_data}
+        q_cat = {g["qid"]: g["category"] for g in gt_data}
 
     assert set(pred.keys()) == set(
         gt.keys()
@@ -46,15 +47,23 @@ def evaluate(args):
         len(pred) == len(gt) == 150
     ), "Prediction and ground truth have wrong number of qids."
 
-    correct = 0
+    correct_cnt = {
+        "all": [],
+        "insurance": [],
+        "finance": [],
+        "faq": []
+    }
     for qid in pred.keys():
         assert (
             pred[qid] in sources[qid]
         ), f"Prediction with qid {qid} does not exist in the sources."
-        if pred[qid] == gt[qid]:
-            correct += 1
+        correct_cnt["all"].append(pred[qid] == gt[qid])
+        correct_cnt[q_cat[qid]].append(pred[qid] == gt[qid])
 
-    print(f"Accuracy: {correct/len(pred):.2f}")
+    print(f"Total Accuracy: {sum(correct_cnt["all"])/len(correct_cnt["all"]):.2f}")
+    print(f"Insurance Accuracy: {sum(correct_cnt["insurance"])/len(correct_cnt["insurance"]):.2f}")
+    print(f"Finance Accuracy: {sum(correct_cnt["finance"])/len(correct_cnt["finance"]):.2f}")
+    print(f"FAQ Accuracy: {sum(correct_cnt["faq"])/len(correct_cnt["faq"]):.2f}")
 
 
 if __name__ == "__main__":
